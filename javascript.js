10dc6f74011dd2c21ebaf43aa325b994301147a3
Ryan Arnett <ryanglennarnett@gmail.com>
2017-05-17 00:56:05 -0500

    some styling

--- javascript.js
+++ javascript.js
@@ -31,27 +31,48 @@ $(document).on("click", "#submitBtn",function()
 $(document).on("click", ".buttons", function () 
 	{
 
+		$("#giphyme").html("");
+
+	
+		
 	var a = this.value;
 	q = a; // search query
+	
 	
 	request = new XMLHttpRequest;
 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
 	
+
+
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			data = JSON.parse(request.responseText).data.image_url;
 			console.log(data);
-			$("#giphyme").html('<center><img src = "'+data+'"  title="GIF via Giphy"></center>');
+			var duh = $('<span><center><img src = "'+data+'"  title="GIF via Giphy"></center></span>');
+			$("#giphyme").append(duh);
+
+		
+	
 		} else {
 			console.log('reached giphy, but API returned an error');
 		 }
 	};
 
+	
+
 	request.onerror = function() {
 		console.log('connection error');
 	};
 
+
 	request.send();
+	
+
+	
+
+
+	
+
 	});
 
 
