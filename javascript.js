d741cc3d6e1c2b7705c3218b58828dd07bc62c9a
Ryan Arnett <ryanglennarnett@gmail.com>
2017-05-16 23:39:56 -0500

    okay so i got soem working code...

--- javascript.js
+++ javascript.js
@@ -11,21 +11,62 @@ for (var i = 0; i < creatures.length; i++)
 	{
 		console.log(i);
 		var r = $("<input type='button' class='buttons' value='"+creatures[i]+"'>");
+
 		$('#buttons').append(r);
 	}
 
 
-function add()
-	{
-	q = $('#input').val().trim(); // search query
+// function add(q)
+// 	{
+// 	//q = $('#input').val().trim(); // search query
+	
+// 	request = new XMLHttpRequest;
+// 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
+// 	data = JSON.parse(request.responseText).data.image_url;
+// 			console.log(data);
+// 			$("#gifs").html('<center><img src = "'+data+'"  title="GIF via Giphy"></center>');
+
+
+// 	}
+
+
+
+
+// $(document.body).on("click", ".buttons", function() {
+
+//       // Get the number of the button from its data attribute and hold in a variable called  toDoNumber.
+//       var value = $(this).attr("value");
+//       add(value);
+//       // Select and Remove the specific <p> element that previously held the to do item number.
+     
+//     });
+
+$(".buttons").on("click", function () {
+	q = "finger guns"; // search query
 	
 	request = new XMLHttpRequest;
 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
-	data = JSON.parse(request.responseText).data.image_url;
+	
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400){
+			data = JSON.parse(request.responseText).data.image_url;
 			console.log(data);
-			$("#gifs").html('<center><img src = "'+data+'"  title="GIF via Giphy"></center>');
+			document.getElementById("giphyme").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
+		} else {
+			console.log('reached giphy, but API returned an error');
+		 }
+	};
+
+	request.onerror = function() {
+		console.log('connection error');
+	};
+
+	request.send();
+});
 
 
-	}
+
+
+
 
 });
